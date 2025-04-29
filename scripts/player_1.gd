57e3bb411f711ee417834591fc767a3ca7cd8c8d
Chimaobi Emmanuel Onwuka <conwuka76@gmail.com>
2025-04-29 17:08:48 -0400

    Added beginning of health system
    Added beginning of health system

--- scripts/player_1.gd
+++ scripts/player_1.gd
@@ -1,36 +1,52 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var health := 3
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var heart_ui = get_node("/root/Game/HeartUI_Player1")
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Apply gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
+	# Jumping
 	if Input.is_action_just_pressed("jump1") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	# Get the input direction: -1, 0, 1
+
+	# Movement input
 	var direction = Input.get_axis("move_left_1", "move_right_1")
-	
-	# Flip the Sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
-	
-	# Apply movement
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	# Collision detection
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		if collider.is_in_group("enemy"):
+			take_damage()
+			collider.queue_free()
+
+func take_damage():
+	health -= 1
+	heart_ui.update_hearts(health)
+	print("Player 1 took damage! Health:", health)
+	if health <= 0:
+		die()
+
+func die():
+	print("Player 1 Died!")
+	queue_free()
