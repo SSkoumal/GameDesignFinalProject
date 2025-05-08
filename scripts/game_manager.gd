2b912961582f820041217d072594c26fee12c352
Simon Skoumal <81614389+SSkoumal@users.noreply.github.com>
2025-05-07 21:56:17 -0400

    levels and splash screen info
    This reverts commit 5d11008e78ec2e0a95811eae4a8ca6b5a3627005, reversing
    changes made to ee85f63dce16075b6a4ca9f1eae3010dfeef0c9c.

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,8 +3,19 @@ extends Node
 var score = 0
 
 @onready var score_label: Label = $ScoreLabel
-
+@onready var level_node: Node = get_parent().get_node("Level1")  # Initially Level1
 
 func add_point():
-		score += 1
-		score_label.text = str(score)
+	score += 1
+	score_label.text = str(score)
+
+	if score == 1:
+		change_level("res://scenes/Level2.tscn")
+	elif score == 2:
+		change_level("res://scenes/Level3.tscn")
+
+func change_level(new_level_path: String):
+	level_node.queue_free()  # Remove the current level
+	var new_level = load(new_level_path).instantiate()
+	get_parent().add_child(new_level)
+	level_node = new_level
